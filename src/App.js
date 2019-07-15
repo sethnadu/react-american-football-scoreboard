b40aeed033606cd8176043b1d66718ae236c8f30
Seth Nadu <seth.nadu@gmail.com>
2019-07-15 18:45:48 -0400

    with objectives

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Objectives from "./Objectives";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -56,7 +57,9 @@ function App() {
        
         </section> 
       </section>
+      <Objectives />
     </div>
+    
   );
 }
 
